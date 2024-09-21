3191372509cca1c652db645d4bebe7b120219489
Beatriz Camara <beatrizslcam@gmail.com>
2024-09-21 07:12:07 -0300

    feat: create the deposit and withdraw

--- domain/account/manage_account.go
+++ domain/account/manage_account.go
@@ -1,3 +1,30 @@
 package account
 
 
+
+func Deposit(id string, amount int) Account{
+	result := GetAccount(id);
+	
+		if result !=  (Account{}) {
+	
+			result.Balance = result.Balance + amount;
+			account_cache[id] = result;
+			return result
+		}
+	
+		return Account{}
+	}
+	
+	func Withdraw(id string, amount int) Account{
+		result := GetAccount(id);
+		
+			if  result != (Account{}) {
+		
+				result.Balance = result.Balance - amount;
+				account_cache[id] = result;
+				return result
+			}
+			
+			return Account{}
+		}
+	
