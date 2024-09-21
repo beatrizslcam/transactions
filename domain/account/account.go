06adc7547b7ffee11ee2b4ddafb809e05ce8e926
Beatriz Camara <beatrizslcam@gmail.com>
2024-09-21 07:11:16 -0300

    feat: created the functions to get and create account and get balance

--- domain/account/account.go
+++ domain/account/account.go
@@ -1,6 +1,7 @@
 package account
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -13,5 +14,44 @@ type Account struct {
 	Created_at  time.Time 
 }
 
+var account_cache = make(map[string]Account)
 
+func CreateAccount(name string, cpf string) Account{
+	if _, ok := account_cache[cpf]; ok {
+		fmt.Print("Account already exists")
+		return account_cache[cpf]
+	}
+	new_account := Account{
+		Name: name,
+		Cpf: cpf,
+		Created_at: time.Now(),
+		Balance: 0,
+		ID: "1",
+		Secret: "123",
+	}
+
+	account_cache[new_account.Cpf] = new_account
+	return  new_account
+}
+
+func GetAccount(cpf string) Account{
+	if result, ok := account_cache[cpf]; ok {
+		return result
+	}
+
+	fmt.Print("Account not found")
+	
+	return Account{}
+	
+}
+
+func GetBalance(id string) int{
+	if result, ok := account_cache[id]; ok {
+		return result.Balance
+	}
+
+	fmt.Print("Account not found")
+	
+	return 0
+}
 
