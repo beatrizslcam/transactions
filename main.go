ca1160692d574c268bf646f8a8b8f2bca8a8b230
Beatriz Camara <beatrizslcam@gmail.com>
2024-09-23 06:37:40 -0300

    feat: testes the usecases on mains

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"transactions/domain/account"
+	"transactions/domain/transfers"
+)
 
+func main() {
+	test := transfers.CreateTransfer("1", "2", 100)
+	fmt.Print(test)
+
+	newaccount := account.CreateAccount("josé", "123456789")
+	fmt.Print(newaccount)
 
 }
